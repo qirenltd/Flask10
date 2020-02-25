240d0e72c6caada7ecdc0841cb38b8273d96a91a
qirencloud@163.com <qirencloud@163.com>
2020-02-26 01:23:04 +0800

    1

--- Flask10/HelloFlask/views.py
+++ Flask10/HelloFlask/views.py
@@ -1,21 +1,38 @@
 from flask import Flask
 from HelloFlask import app
+from datetime import datetime
+from flask import render_template
+
 
 @app.route('/')
-@app.route('/home')
-def home():
-    return "企人"
+@app.route('/qiren')
+def qiren():
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    html_content = "<html><head><title>企人</title></head><body>"
+    html_content += "<strong>企人中心</strong> on " + formatted_now
+    html_content += "</body></html>"
+
+    return html_content
 
 @app.route('/liao')
 def liao():
-    return "企人聊" 
+    return "<html><head><title>企人聊</title></head>企人聊" 
 
 @app.route('/sou')
 def sou():
-    return "企人搜"
+    #render_template 加载模板
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    return "企人搜"+render_template(
+        "index.html",
+        content = "<strong>Hello, Flask!</strong> on " + formatted_now)
 
 @app.route('/buy')
 def buy():
-    return "企人买"
-
-
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    b="企人买"
+    a="买"
+    return render_template("index.html",content=formatted_now,title=a,message=b)
