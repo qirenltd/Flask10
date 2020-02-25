a411cf97710ade18b425862c9294b631133391d4
qirencloud@163.com <qirencloud@163.com>
2020-02-26 00:44:45 +0800

    0.2

--- Flask10/HelloFlask/views.py
+++ Flask10/HelloFlask/views.py
@@ -18,12 +18,4 @@ def sou():
 def buy():
     return "企人买"
 
-if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
 
